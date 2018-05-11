ee0ba3373463026a3057ff5068245c4ebb1dbb40
Garenge <garenge@outlook.com>
2018-05-11 17:49:26 +0800

    Initial Commit

---
+++ PPSecureTextFieldDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PPSecureTextFieldDemo
+//
+//  Created by istLZP on 2018/5/11.
+//  Copyright © 2018年 Garenge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
